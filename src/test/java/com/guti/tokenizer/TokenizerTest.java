f29acc52227700b96687c19ad1be52166134e59f
Mattia Gualtieri <mattiagualtieri2000@gmail.com>
2025-11-22 11:04:46 +0100

    Tests for Tokenizer

--- src/test/java/com/guti/tokenizer/TokenizerTest.java
+++ src/test/java/com/guti/tokenizer/TokenizerTest.java
@@ -1,16 +1,22 @@
 package com.guti.tokenizer;
 
+import static com.guti.tokenizer.word.DateKeywordWord.DateKeyword.*;
+import static com.guti.tokenizer.word.KeywordWord.Keyword.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import com.guti.normalizer.pipeline.DefaultNormalizationPipeline;
+import com.guti.tokenizer.word.MeridiemWord;
 import com.guti.tokenizer.word.WordTokenizer;
+
+import java.time.DayOfWeek;
+import java.time.LocalTime;
+import java.time.Month;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+import java.util.stream.Stream;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-
-import java.util.List;
-import java.util.stream.Stream;
-
-import static com.guti.tokenizer.word.DateKeywordWord.DateKeyword.*;
-import static org.junit.jupiter.api.Assertions.*;
 
 class TokenizerTest {
 
@@ -30,8 +36,53 @@ class TokenizerTest {
   private static Stream<Arguments> provideInputsForShouldTokenize() {
     return Stream.of(
         Arguments.of("Today", List.of(new Token(TokenType.DATE_KEYWORD, "today", TODAY))),
-        Arguments.of("Tomorrow", List.of(new Token(TokenType.DATE_KEYWORD, "tomorrow", TOMORROW))),
         Arguments.of(
-            "Yesterday", List.of(new Token(TokenType.DATE_KEYWORD, "yesterday", YESTERDAY))));
+            "Tomorrow at 5pm",
+            List.of(
+                new Token(TokenType.DATE_KEYWORD, "tomorrow", TOMORROW),
+                new Token(TokenType.KEYWORD, "at", AT),
+                new Token(TokenType.TIME, "5pm", LocalTime.of(17, 0)))),
+        Arguments.of(
+            "two days ago",
+            List.of(
+                new Token(TokenType.NUMBER, "2", 2),
+                new Token(TokenType.UNIT, "days", ChronoUnit.DAYS),
+                new Token(TokenType.KEYWORD, "ago", AGO))),
+        Arguments.of(
+            "Day after tomorrow",
+            List.of(new Token(TokenType.DATE_KEYWORD, "day_after_tomorrow", DAY_AFTER_TOMORROW))),
+        Arguments.of(
+            "Sunday, March 3rd 2024",
+            List.of(
+                new Token(TokenType.WEEKDAY, "sunday", DayOfWeek.SUNDAY),
+                new Token(TokenType.MONTH, "march", Month.MARCH),
+                new Token(TokenType.NUMBER, "3rd", 3),
+                new Token(TokenType.YEAR, "2024", 2024))),
+        Arguments.of(
+            "AT MIDNIGHT",
+            List.of(
+                new Token(TokenType.KEYWORD, "at", AT),
+                new Token(TokenType.TIME_KEYWORD, "midnight", LocalTime.MIDNIGHT))),
+        Arguments.of(
+            "5 p.m.",
+            List.of(
+                new Token(TokenType.NUMBER, "5", 5),
+                new Token(TokenType.MERIDIEM, "p.m.", MeridiemWord.Meridiem.PM))),
+        Arguments.of(
+            "twenty-one minutes",
+            List.of(
+                new Token(TokenType.NUMBER, "21", 21),
+                new Token(TokenType.UNIT, "minutes", ChronoUnit.MINUTES))),
+        Arguments.of(
+            "  Tomorrow   at   noon  ",
+            List.of(
+                new Token(TokenType.DATE_KEYWORD, "tomorrow", TOMORROW),
+                new Token(TokenType.KEYWORD, "at", AT),
+                new Token(TokenType.TIME_KEYWORD, "noon", LocalTime.NOON))),
+        Arguments.of(
+            "random!!! word",
+            List.of(
+                new Token(TokenType.UNKNOWN, "random", "random"),
+                new Token(TokenType.UNKNOWN, "word", "word"))));
   }
 }
